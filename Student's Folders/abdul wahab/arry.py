eecf90974647d0f40280590cbae9961f569982ea
Abdul wahab <muhammadibrahim18905>
2025-10-17 22:44:25 +0500

    function

---
+++ Student's Folders/abdul wahab/arry.py
@@ -0,0 +1,14 @@
+"""Write a C program that reads an integer between 1 and 12 and prints the month of the year in English.
+Test Data"""
+
+month = int(input("Input a number between 1 and 12: "))
+
+
+
+
+
+
+
+
+
+
