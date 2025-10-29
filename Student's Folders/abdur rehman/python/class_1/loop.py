ac38de1953fae14d8ff2da092ad5a0f5301f857e
smart-tech202412 <abdurrehman202412@gmail.com>
2025-10-29 21:14:17 +0500

    web scraping

---
+++ Student's Folders/abdur rehman/python/class_1/loop.py
@@ -0,0 +1,11 @@
+numbers =[]
+positive = 0 
+negative = 0
+for i in range(0,5,1):
+    userinput = int(input("enter any digit"))
+    numbers.append(numbers)
+for y in range(5):
+    if numbers[y]>0:
+        print("these are positive number",numbers[y],end="")    
+    else:
+        print("these are negative number",numbers [y],end="")    
---
+++ Student's Folders/abdur rehman/python/class_1/neastedforloop.py
@@ -0,0 +1,62 @@
+#Abdur  rehman shakir
+# for i in range(1,5):
+#     for j in range(i):
+#         print(i, end=" ")
+#     print() 
+
+
+# for i in range(4,0,-1):
+#     for j in range(i):
+#         print(i, end=" ")
+#     print() 
+
+
+# for i in range(1,4):
+#     for j in range(3):
+#         print(f"{i,j}", end=" ")
+#     print()    
+
+
+# for i in range(1,3+1):
+#     for j in range(1,i+1):
+#         print(j, end=" ")
+#     print()    
+
+
+# for i in range(3,0,-1):
+#     for j in range(i):
+#         print(j,end=" ")
+#     print()
+
+
+# for i in range(1,4,1):
+#     for j in range(1,4):
+#         print(f"{i*j}" , end=" ")
+#     print()    
+
+
+
+# row = 5
+# num = 1
+# for i in range(1,row):
+#     for j in range(i):
+#         print(num,end=" ")
+#         num = num+1
+#     print()    
+
+
+# row = 5
+# num = 1
+# for i in range(1,row):
+#     for j in range(i):
+#         print(num, end=" ")
+#         num = num+2
+#     print()    
+# print(chr(65))
+# row =5
+# num = 2
+# for i in range(1,row):
+#     for j in range(i):
+#         print(num, end=" ")
+#         num = num + 2
+#     print() 
